33773e24ec3ef3a13a5180b6ed08e5b3080f0f88
Tom <Tom@Mac.lan>
2025-10-25 16:36:21 +0300

    chore: add next.config for static export

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
