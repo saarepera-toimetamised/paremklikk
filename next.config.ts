2dbe1075f095409aaa62e9f000ea1f0d9c40d697
Tom <Tom@Mac.lan>
2025-10-25 14:07:53 +0300

    Add GitHub Pages workflow + static export

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+const nextConfig = {
+  output: 'export',
 };
 
 export default nextConfig;
